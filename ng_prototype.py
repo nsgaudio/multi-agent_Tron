174b982c1afadf8bea850bdd45a66bdad78d7e2d
Nicholas Gaudio <nsgaudio@stanford.edu>
2020-02-18 14:55:57 -0800

    .

---
+++ ng_prototype.py
@@ -0,0 +1,119 @@
+import os
+import random
+import numpy as np
+# import torch
+
+from matplotlib import pyplot as plt
+from matplotlib import animation
+from matplotlib import colors
+
+class TronEnvironment:
+    def __init__(self, config):
+        self.config = config
+        self.board  = self.clear_board()
+        self.cycles = self.init_cycles()
+        self.t      = 0
+        
+    def clear_board(self):
+        board = np.zeros((self.config.size, self.config.size))
+        return board
+
+    def init_cycles(self):
+        '''Creates the cycles matrix of form:
+            Head -> [[c1y, c1y, c1y, ...] <- Tail
+                    [c1x, c1x, c1x, ...]
+                    [c2y, c2y, c2y, ...]
+                    [c2x, c2x, c2x, ...]]'''
+        cycles = np.zeros((2*self.config.num_cycle, self.config.cycle_len))
+
+        for c in range(1, self.config.num_cycle+1):
+            y_2 = int(self.config.size/2)
+            cycles[2*c-2, :] = np.array(range(y_2, y_2-self.config.cycle_len, -1))
+            cycles[2*c-1, :] = int(c*self.config.size/(self.config.num_cycle+1))
+            cycles = cycles.astype(int)
+        print('Cycles matrix:\n', cycles)
+        return cycles
+
+    def valid_move(self, coord):
+        '''Checks if the move is valid iff: 
+            (1) in the board bounds.
+            (2) the space is empty.'''
+        return np.all(np.greater(coord, -1)) and \
+            np.all(np.less(coord, self.config.size)) and \
+            self.board[coord[0], coord[1]] == 0  # 0 means empty state
+
+    def update_cycles(self):
+        self.board = self.make_board()
+        s_p = np.zeros((self.cycles.shape[0], 1))
+
+        for c in range(1, self.config.num_cycle+1):
+            head      = self.cycles[2*c-2:2*c, 0]
+            behind    = self.cycles[2*c-2:2*c, 1]
+            direction = head - behind
+            forward   = head + direction
+
+            if self.valid_move(coord=forward):
+                selected = forward
+            else:
+                possible = []
+                up    = [head[0]-1, head[1]]
+                down  = [head[0]+1, head[1]]
+                left  = [head[0], head[1]-1]
+                right = [head[0], head[1]+1]
+                if self.valid_move(coord=up): possible.append(up)
+                if self.valid_move(coord=down): possible.append(down)
+                if self.valid_move(coord=left): possible.append(left)
+                if self.valid_move(coord=right): possible.append(right)
+                possible = np.array(possible)
+                print('possible shape:', possible.shape)
+                print(possible)
+                selected = possible[np.random.randint(0, high=possible.shape[0]), :]
+            print('selected:', selected)
+            s_p[2*c-2:2*c] = np.expand_dims(selected, axis=-1)
+
+        self.cycles = np.append(s_p, self.cycles, axis=1).astype(int)
+        if  not (self.t%self.config.lengthen_every == 0):
+            self.cycles = np.delete(self.cycles, -1, axis=1)
+        self.t += 1
+        print('Cycles matrix:\n', self.cycles)
+        return self.cycles
+
+    def make_board(self):
+        self.board = self.clear_board()
+        for c in range(1, self.config.num_cycle+1):
+            self.board[self.cycles[2*c-2, :], self.cycles[2*c-1, :]] = c
+        return self.board
+
+    def show_board(self):
+        '''Makes a plot of the game board.'''
+        fig = plt.figure()
+        plt.pcolor(self.board, cmap=self.config.cmap, edgecolors='k', linewidths=1)
+        if config.show:
+            plt.draw()
+            plt.pause(0.025)
+            plt.clf()
+        if self.config.file_name is not None:
+            fig.savefig(self.config.file_name)
+
+class Config:
+    def __init__(self):
+        self.size           = 25
+        self.num_cycle      = 4
+        self.cycle_len      = 7
+        self.show           = True
+        self.time_steps     = 1000
+        self.lengthen_every = 2
+        self.cycle_colors   = ['white', 'red', 'blue', 'green', 'orange', 'purple']  # white is board color, others are cycles
+        self.cmap           = colors.ListedColormap(self.cycle_colors[:self.num_cycle+1])
+        self.file_name      = None
+
+if __name__ == '__main__':
+    config = Config()
+    tron = TronEnvironment(config)
+
+    tron.make_board()
+    tron.show_board()
+    for t in range(config.time_steps):
+        cycles = tron.update_cycles()
+        board  = tron.make_board()
+        tron.show_board()
--- ng_tron.py
+++ ng_tron.py
@@ -7,49 +7,57 @@ from matplotlib import pyplot as plt
 from matplotlib import animation
 from matplotlib import colors
 
+class Config:
+    def __init__(self):
+        self.size           = 25
+        self.num_cycle      = 4
+        self.cycle_len      = 7
+        self.show           = True
+        self.time_steps     = 1000
+        self.lengthen_every = 2
+        self.cycle_colors   = ['white', 'red', 'blue', 'green', 'orange', 'purple']  # white is board color, others are cycles
+        self.cmap           = colors.ListedColormap(self.cycle_colors[:self.num_cycle+1])
+        self.file_name      = None
 
-def init_board(size):
-    board = np.zeros((size, size))
+def init_board(config):
+    board = np.zeros((config.size, config.size))
     return board
 
-
-def valid_move(size, coord, board):
-    '''Checks if the move is valid: 
-        (1) in the board bounds.
-        (2) the space id empty.'''
-    return np.all(np.greater(coord, -1)) and \
-           np.all(np.less(coord, size)) and \
-           board[coord[0], coord[1]] == 0  # 0 means empty state
-
-
-def init_cycles(size, num_cycle, cycle_len):
+def init_cycles(config):
     '''Creates the cycles matrix of form:
         Head -> [[c1y, c1y, c1y, ...] <- Tail
                  [c1x, c1x, c1x, ...]
                  [c2y, c2y, c2y, ...]
                  [c2x, c2x, c2x, ...]]'''
-    cycles = np.zeros((2*num_cycle, cycle_len))
+    cycles = np.zeros((2*config.num_cycle, config.cycle_len))
 
-    for c in range(1, num_cycle+1):
-        y_2 = int(size/2)
-        cycles[2*c-2, :] = np.array(range(y_2, y_2-cycle_len, -1))
-        cycles[2*c-1, :] = int(c*size/(num_cycle+1))
+    for c in range(1, config.num_cycle+1):
+        y_2 = int(config.size/2)
+        cycles[2*c-2, :] = np.array(range(y_2, y_2-config.cycle_len, -1))
+        cycles[2*c-1, :] = int(c*config.size/(config.num_cycle+1))
         cycles = cycles.astype(int)
     print('Cycles matrix:\n', cycles)
     return cycles
 
+def valid_move(config, coord, board):
+    '''Checks if the move is valid iff: 
+        (1) in the board bounds.
+        (2) the space is empty.'''
+    return np.all(np.greater(coord, -1)) and \
+           np.all(np.less(coord, config.size)) and \
+           board[coord[0], coord[1]] == 0  # 0 means empty state
 
-def update_cycles(size, cycles, num_cycle):
-    board = make_board(size=size, cycles=cycles, num_cycle=num_cycle)
+def update_cycles(config, cycles, t):
+    board = make_board(config, cycles=cycles)
     s_p = np.zeros((cycles.shape[0], 1))
 
-    for c in range(1, num_cycle+1):
+    for c in range(1, config.num_cycle+1):
         head  = cycles[2*c-2:2*c, 0]
         behind = cycles[2*c-2:2*c, 1]
         direction = head - behind
         forward = head + direction
 
-        if valid_move(size=size, coord=forward, board=board):
+        if valid_move(config, coord=forward, board=board):
             selected = forward
         else:
             possible = []
@@ -57,10 +65,10 @@ def update_cycles(size, cycles, num_cycle):
             down  = [head[0]+1, head[1]]
             left  = [head[0], head[1]-1]
             right = [head[0], head[1]+1]
-            if valid_move(size=size, coord=up, board=board): possible.append(up)
-            if valid_move(size=size, coord=down, board=board): possible.append(down)
-            if valid_move(size=size, coord=left, board=board): possible.append(left)
-            if valid_move(size=size, coord=right, board=board): possible.append(right)
+            if valid_move(config, coord=up, board=board): possible.append(up)
+            if valid_move(config, coord=down, board=board): possible.append(down)
+            if valid_move(config, coord=left, board=board): possible.append(left)
+            if valid_move(config, coord=right, board=board): possible.append(right)
             possible = np.array(possible)
             print('possible shape:', possible.shape)
             print(possible)
@@ -69,45 +77,35 @@ def update_cycles(size, cycles, num_cycle):
         s_p[2*c-2:2*c] = np.expand_dims(selected, axis=-1)
 
     cycles = np.append(s_p, cycles, axis=1).astype(int)
-    cycles = np.delete(cycles, -1, axis=1)
+    if  not (t%config.lengthen_every == 0):
+        cycles = np.delete(cycles, -1, axis=1)
     print('Cycles matrix:\n', cycles)
     return cycles
 
-
-def make_board(size, cycles, num_cycle):
-    board = init_board(size=size)
-    for c in range(1, num_cycle+1):
+def make_board(config, cycles):
+    board = init_board(config)
+    for c in range(1, config.num_cycle+1):
         board[cycles[2*c-2, :], cycles[2*c-1, :]] = c
     return board
 
-
-def show_board(file_name, show, board, num_cycle):
+def show_board(config, board):
     '''Makes a plot of the game board.'''
     fig = plt.figure()
-    cycle_colors = ['white', 'red', 'blue', 'green', 'orange', 'purple']  # white is board color, others are cycles
-    cycle_colors = cycle_colors[:num_cycle+1]
-    cmap = colors.ListedColormap(cycle_colors)
-    plt.pcolor(board, cmap=cmap, edgecolors='k', linewidths=1)
-    if show:
+    plt.pcolor(board, cmap=config.cmap, edgecolors='k', linewidths=1)
+    if config.show:
         plt.draw()
-        plt.pause(0.25)
+        plt.pause(0.025)
         plt.clf()
-    if file_name is not None:
-        fig.savefig(file_name)
-
+    if config.file_name is not None:
+        fig.savefig(config.file_name)
 
 if __name__ == '__main__':
-    size       = 25
-    num_cycle  = 4
-    cycle_len  = 7
-    show       = True
-    time_steps = 100
+    config = Config()
+    cycles = init_cycles(config)
+    board  = make_board(config, cycles=cycles)
+    show_board(config, board=board)
 
-    cycles = init_cycles(size=size, num_cycle=num_cycle, cycle_len=cycle_len)
-    board = make_board(size=size, cycles=cycles, num_cycle=num_cycle)
-    show_board(file_name=None, show=show, board=board, num_cycle=num_cycle)
-
-    for t in range(time_steps):
-        cycles = update_cycles(size, cycles, num_cycle)
-        board = make_board(size=size, cycles=cycles, num_cycle=num_cycle)
-        show_board(file_name=None, show=True, board=board, num_cycle=num_cycle)
+    for t in range(config.time_steps):
+        cycles = update_cycles(config, cycles, t=t)
+        board  = make_board(config, cycles=cycles)
+        show_board(config, board=board)
