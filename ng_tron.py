21aa4f696bc376eda3e49d21f5366ca007de46b9
Nicholas Gaudio <nsgaudio@stanford.edu>
2020-02-17 23:21:49 -0800

    .

--- ng_tron.py
+++ ng_tron.py
@@ -12,6 +12,12 @@ def init_board(size):
     board = np.zeros((size, size))
     return board
 
+def valid_move(size, coord, board):
+    if coord[0] > -1 and coord[1] > -1 and coord[0] < size and coord[1] < size and board[coord[0], coord[1]] == 0:
+        return True
+    else:
+        return False
+
 
 def init_cycles(size, num_cycle, cycle_len):
     '''Creates the cycles matrix of form:
@@ -30,27 +36,80 @@ def init_cycles(size, num_cycle, cycle_len):
     return cycles
 
 
-def show_board(file_name, cycles, board, num_cycle):
+def update_cycles(size, cycles, num_cycle):
+    board = make_board(size=size, cycles=cycles, num_cycle=num_cycle)
+    s_p = np.zeros((cycles.shape[0], 1))
+
+    for c in range(1, num_cycle+1):
+        possible = []
+        head  = cycles[2*c-2:2*c, 0]
+        print('head', head[1])
+
+        up    = [head[0]-1, head[1]]
+        down  = [head[0]+1, head[1]]
+        left  = [head[0], head[1]-1]
+        right = [head[0], head[1]+1]
+        print('up', up)
+        
+        a = 0
+
+        if valid_move(size=size, coord=up, board=board): possible.append(up)
+        if valid_move(size=size, coord=down, board=board): possible.append(down)
+        if valid_move(size=size, coord=left, board=board): possible.append(left)
+        if valid_move(size=size, coord=right, board=board): possible.append(right)
+        possible = np.array(possible)
+        print('possible:', possible.shape)
+        print(possible)
+        selected = possible[np.random.randint(0, high=possible.shape[0]), :]
+        print('selected', selected)
+        s_p[2*c-2:2*c] = np.expand_dims(selected, axis=-1)
+
+
+
+
+    print(cycles)
+    cycles = np.append(s_p, cycles, axis=1).astype(int)
+    cycles = np.delete(cycles, -1, axis=1)
+    print(cycles)
+    return cycles
+
+
+def make_board(size, cycles, num_cycle):
+    board = init_board(size=size)
+    for c in range(1, num_cycle+1):
+        board[cycles[2*c-2, :], cycles[2*c-1, :]] = c
+    return board
+
+
+def show_board(file_name, show, board, num_cycle):
     '''Makes a plot of the game board.'''
     fig = plt.figure()
-
     cycle_colors = ['white','red', 'blue', 'green', 'orange', 'purple']  # white is board color, others are cycles
     cycle_colors = cycle_colors[:num_cycle+1]
     cmap = colors.ListedColormap(cycle_colors)
-
-    for c in range(1, num_cycle+1):
-        board[cycles[2*c-2, :], cycles[2*c-1, :]] = c
-    plt.pcolor(board,cmap=cmap,edgecolors='k', linewidths=1)
-    plt.show()
+    plt.pcolor(board, cmap=cmap, edgecolors='k', linewidths=1)
+    if show:
+        # plt.show()
+        plt.draw()
+        plt.pause(0.25)
+        plt.clf()
     if file_name is not None:
         fig.savefig(file_name)
 
+
 if __name__ == '__main__':
-    size      = 25
-    num_cycle = 2
-    cycle_len = 7
+    size       = 25
+    num_cycle  = 4
+    cycle_len  = 7
+    show       = True
+    time_steps = 100
 
-    board = init_board(size=size)
     cycles = init_cycles(size=size, num_cycle=num_cycle, cycle_len=cycle_len)
-    show_board(file_name=None, cycles=cycles, board=board, num_cycle=num_cycle)
+    board = make_board(size=size, cycles=cycles, num_cycle=num_cycle)
+    show_board(file_name=None, show=show, board=board, num_cycle=num_cycle)
 
+
+    for t in range(time_steps):
+        cycles = update_cycles(size, cycles, num_cycle)
+        board = make_board(size=size, cycles=cycles, num_cycle=num_cycle)
+        show_board(file_name=None, show=True, board=board, num_cycle=num_cycle)
