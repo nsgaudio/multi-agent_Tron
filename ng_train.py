039a76bc31fbf73397f58c7c6386c4861f53f55a
Nicholas Gaudio <nsgaudio@stanford.edu>
2020-02-24 20:54:26 -0800

    .

--- ng_train.py
+++ ng_train.py
@@ -27,25 +27,22 @@ class InputStack(object):
     def __init__(self, env):
         self.input_stack = np.zeros((env.board_shape[0], env.board_shape[1], 2 * env.config.INPUT_FRAME_NUM))
         observation, head_board, _ = env.init_board()
-        print('INITIAL INPUT STACK:')
-        for i in range(2 * env.config.INPUT_FRAME_NUM):
-            if np.mod(i, 2) == 0:
-                self.input_stack[:, :, i] = observation
-                print(self.input_stack[:, :, i])
+        for c in range(2 * env.config.INPUT_FRAME_NUM):
+            if np.mod(c, 2) == 0:
+                self.input_stack[:, :, c] = observation
             else:
-                self.input_stack[:, :, i] = head_board
-                print(self.input_stack[:, :, i])
+                self.input_stack[:, :, c] = head_board
     def update(self, env):
+        self.input_stack = np.append(np.expand_dims(env.head_board, axis=-1), self.input_stack, axis=-1)
+        self.input_stack = np.append(np.expand_dims(env.observation, axis=-1), self.input_stack, axis=-1)
+        self.input_stack = np.delete(self.input_stack, -1, axis=-1)
+        self.input_stack = np.delete(self.input_stack, -1, axis=-1)
 
 
-
-        
-
-
-class DQN(nn.Module):
+class Tron_DQN(nn.Module):
 
     def __init__(self, h, w, outputs):
-        super(DQN, self).__init__()
+        super(Tron_DQN, self).__init__()
         self.conv1 = nn.Conv2d(3, 16, kernel_size=5, stride=2)
         self.bn1 = nn.BatchNorm2d(16)
         self.conv2 = nn.Conv2d(16, 32, kernel_size=5, stride=2)
@@ -70,48 +67,18 @@ class DQN(nn.Module):
         x = F.relu(self.bn3(self.conv3(x)))
         return self.head(x.view(x.size(0), -1))
 
-
-input_stack = InputStack(env)
-
-print('input_stack shape:', input_stack.input_stack.shape)
-print('obs shape:', env.observation.shape)
-
 env.reset()
 env.render()
 
-# BATCH_SIZE = 128
-# GAMMA = 0.999
-# EPS_START = 0.9
-# EPS_END = 0.05
-# EPS_DECAY = 200
-# TARGET_UPDATE = 10
+input_stack = InputStack(env)
+# input_stack.update(env)
 
-config = Config()  # added
-
-# Get screen size so that we can initialize layers correctly based on shape
-# returned from AI gym. Typical dimensions at this point are close to 3x40x90
-# which is the result of a clamped and down-scaled render buffer in get_screen()
-# init_screen = get_screen()  # commented
-
-# _, _, screen_height, screen_width = init_screen.shape  # commented
-screen_height = env.board_shape[0]  # added
-screen_width = env.board_shape[1]  # added
-
-# screen_height = 100  # added
-# screen_width = 100  # added
-
-# Get number of actions from gym action space
-n_actions = env.action_space.n
-
-
-print('see:   ', screen_height, screen_width, n_actions)
-policy_net = DQN(screen_height, screen_width, n_actions).to(device)
-target_net = DQN(screen_height, screen_width, n_actions).to(device)
+policy_net = Tron_DQN(h=env.board_shape[0], w=env.board_shape[1], outputs=env.action_space.n).to(device)
+target_net = Tron_DQN(h=env.board_shape[0], w=env.board_shape[1], outputs=env.action_space.n).to(device)
 target_net.load_state_dict(policy_net.state_dict())
 target_net.eval()
 
 optimizer = optim.RMSprop(policy_net.parameters())
-memory = ReplayMemory(10000)
 
 
 steps_done = 0
@@ -224,7 +191,7 @@ for i_episode in range(num_episodes):
             episode_durations.append(t + 1)
             plot_durations()
             break
-    # Update the target network, copying all weights and biases in DQN
+    # Update the target network, copying all weights and biases in Tron_DQN
     if i_episode % env.config.TARGET_UPDATE == 0:
         target_net.load_state_dict(policy_net.state_dict())
 
